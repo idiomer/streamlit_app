32bef89d52578c4141609a1b9fcfbda9c3c40e4e
idiomer <treasuresss@126.com>
2023-03-30 20:34:33 +0800

    cache: fig

--- pages/10_📈_Finance.py
+++ pages/10_📈_Finance.py
@@ -15,11 +15,8 @@ st.title('My Finance Playground')
 st.header('〇、想法')
 st.checkbox("""将未来N天的盈利天数占比做为标签预测""")
 
-# 1. 指数历史走势
-st.header('一、指数历史走势')
-stock = Stock(stock_db_path)
-stock2name = {'sh.000300': '沪深300', 'sh.000905': '中证500'}
-for stock_code, stock_name in stock2name.items():
+@st.cache_data(persist="disk")
+def load_stock_to_fig(stock_code):
     base_df = stock.load_stock(stock_code)  # 从db或网络加载数据
     base_df['近5年25分位'] = base_df['close'].rolling(1200).apply(lambda x: np.percentile(x, q=25))
     base_df['近5年50分位'] = base_df['close'].rolling(1200).median()
@@ -29,4 +26,13 @@ for stock_code, stock_name in stock2name.items():
     fig = px.line(base_df, x="日期", y=["收盘价", "近5年25分位", "近5年50分位", "近5年75分位"]
         ,title=stock_name+"历史走势")
     fig.update_xaxes(nticks=40, tickformat="%Y-%m-%d", tickangle=45)
+    return fig
+
+# 1. 指数历史走势
+st.header('一、指数历史走势')
+stock = Stock(stock_db_path)
+stock2name = {'sh.000300': '沪深300', 'sh.000905': '中证500'}
+for stock_code, stock_name in stock2name.items():
+    fig = load_stock_to_fig(stock_code)
     st.plotly_chart(fig, use_container_width=True, theme='streamlit')
+
